0556b3446aaac08a0846e4344aba18bcd46a0ae2
nityanandagohain <nityanandagohain@gmail.com>
2022-12-08 12:16:59 +0530

    fix: logs parser test file updated

--- pkg/query-service/app/logs/parser_test.go
+++ pkg/query-service/app/logs/parser_test.go
@@ -367,33 +367,3 @@ func TestGenerateSQLQuery(t *testing.T) {
 		})
 	}
 }
-
-// func TestGenerateSQLQueryCaseSensitivity(t *testing.T) {
-// 	allFields := model.GetFieldsResponse{
-// 		Selected: []model.LogField{
-// 			{
-// 				Name:     "OtherField",
-// 				DataType: "int64",
-// 				Type:     "attributes",
-// 			},
-// 		},
-// 		Interesting: []model.LogField{
-// 			{
-// 				Name:     "Code",
-// 				DataType: "int64",
-// 				Type:     "attributes",
-// 			},
-// 		},
-// 	}
-
-// 	query := "otherfield lt 100 and otherField gt 50 AND Code lte 500 and code gte 400"
-// 	tsStart := uint64(1657689292000)
-// 	tsEnd := uint64(1657689294000)
-// 	idStart := "2BsKLKv8cZrLCn6rkOcRGkdjBdM"
-// 	idEnd := "2BsKG6tRpFWjYMcWsAGKfSxoQdU"
-// 	sqlWhere := "timestamp >= '1657689292000' and timestamp <= '1657689294000' and id > '2BsKLKv8cZrLCn6rkOcRGkdjBdM' and id < '2BsKG6tRpFWjYMcWsAGKfSxoQdU' and OtherField < 100 and OtherField > 50 AND attributes_int64_value[indexOf(attributes_int64_key, 'Code')] <= 500 and attributes_int64_value[indexOf(attributes_int64_key, 'Code')] >= 400 "
-// 	Convey("TestGenerateSQLQueryCaseSensitivity", t, func() {
-// 		res, _ := GenerateSQLWhere(&allFields, &model.LogsFilterParams{Query: query, TimestampStart: tsStart, TimestampEnd: tsEnd, IdGt: idStart, IdLT: idEnd})
-// 		So(res, ShouldEqual, sqlWhere)
-// 	})
-// }
